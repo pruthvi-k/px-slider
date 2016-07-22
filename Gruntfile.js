ee7ae7a2061b48159c3b4198ced24d1cdb2fde2d
212417951 <katherine.menkhaus@ge.com>
2016-07-21 20:33:47 -0700

    mega demo progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,10 +23,10 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                  // 'css/noprefix/px-handle.css': 'sass/px-handle.scss',
                   'css/noprefix/px-slider-sketch.css': 'sass/px-slider-sketch.scss',
                   'css/noprefix/px-slider.css': 'sass/px-slider-predix.scss',
                   'css/noprefix/px-slider-demo.css': 'sass/px-slider-demo.scss'
+                  // 'css/noprefix/px-handle.css': 'sass/px-handle.scss'
                 }
             }
         },
